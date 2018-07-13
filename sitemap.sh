ce170e5fa4d7ae210c8c961eb44a131c584ae27a
Xantorohara <xantorohara@gmail.com>
2018-07-13 17:39:38 +0300

    fixed link to linux notes

--- sitemap.sh
+++ sitemap.sh
@@ -1,1 +1,3 @@
-cat README.md | grep "https://" | cut -d "(" -f 2 |cut -d ")" -f 1 >sitemap.txt
+#!/usr/bin/env bash
+
+cat README.md | grep "(https://" | cut -d "(" -f 2 |cut -d ")" -f 1 >sitemap.txt
